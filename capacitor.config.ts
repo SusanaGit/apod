c37405d3d6e6366f0cb0fbeb0de88c92c1efdbc1
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-07-08 18:29:59 +0200

    change app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'apod',
+  appName: 'APOD',
   webDir: 'www'
 };
 
