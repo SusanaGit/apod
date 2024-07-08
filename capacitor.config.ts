3cd35599f0b3a04b2f834a0bb05e6283af486e95
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-07-08 20:21:06 +0200

    enable CapacitorHttp

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'APOD',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    CapacitorHttp: {
+      enabled: true
+    }
+  }
 };
 
 export default config;
